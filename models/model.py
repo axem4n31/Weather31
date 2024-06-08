ec3355c8806362ec4b37f6c22c3f3a97a4ec8d7d
Toporov Denis <toporov.den2@list.ru>
2024-06-08 15:29:55 +0300

    refact alembic

--- models/model.py
+++ models/model.py
@@ -1,6 +1,7 @@
 from model_settings import Base
 from sqlalchemy import Column, Integer, String, Float, JSON
 
+
 class User(Base):
     __tablename__ = 'User'
     user_tg_id = Column(Integer, primary_key=True)
