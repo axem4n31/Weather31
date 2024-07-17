a61d63617640a25de4485e7df52decedf199b5f9
Toporov Denis <toporov.den2@list.ru>
2024-07-17 22:01:33 +0300

    returned to normal view (removed the celery call)

--- telegram_service/handly_event.py
+++ telegram_service/handly_event.py
@@ -2,9 +2,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.status import HTTP_200_OK
 from fastapi import Request, HTTPException, Depends
-from datetime import datetime, timedelta
-from tasks import add
-
 import settings
 from models.model_settings import db_helper
 from telegram_service.events import start_event, help_event, registration_event, change_region_event, forecast_event, \
@@ -28,37 +25,41 @@ events_without_db = {
 
 async def handle_bot_events(request: Request, secret_key: str,
                             db: AsyncSession = Depends(db_helper.scoped_session_dependency)):
+    """
+    The function checks the user's response and triggers the corresponding event
+    """
     # Проверка секретного ключа и метода запроса
     check_secret_key(secret_key=secret_key)
     check_method(request=request)
 
-    # Получение данных из запроса
-    message = await request.json()
-    print(message)
+    try:
+        # Получение данных из запроса
+        message = await request.json()
+        print(message)
+        if 'message' in message:
+            # Если в сообщении есть текст, обрабатываем его
+            if 'text' in message['message']:
+                text = message['message']['text']
+                chat_id = int(message['message']['chat']['id'])
+                user_tg_id = int(message['message']['from']['id'])
+                if text in events_with_db:
+                    await events_with_db[text](chat_id, user_tg_id, db=db)
+                elif text in events_without_db:
+                    await events_without_db[text](chat_id)
+                else:
+                    await registration_event(message=message, db=db)
 
-    if 'message' in message:
-        # Если в сообщении есть текст, обрабатываем его
-        if 'text' in message['message']:
-            text = message['message']['text']
-            if text in events_with_db:
-                await events_with_db[text](message, db=db)
-            elif text in events_without_db:
-                # await events_without_db[text](message)
-                eta_time = datetime.now() + timedelta(seconds=5) - timedelta(hours=3)
-                print(eta_time)
-                add.apply_async(args=[message], eta=eta_time)
-            else:
-                await registration_event(message=message, db=db)
+            # Если в сообщении есть местоположение, вызываем соответствующую функцию
+            elif 'location' in message['message']:
+                await get_coordinates_from_user_event(message, db=db)
 
-        # Если в сообщении есть местоположение, вызываем соответствующую функцию
-        elif 'location' in message['message']:
-            await get_coordinates_from_user_event(message, db=db)
+        elif 'callback_query' in message and 'data' in message['callback_query']:
+            # Если в запросе есть callback_query с данными, вызываем by_hourly_event
+            await by_hourly_event(message, db=db)
 
-    elif 'callback_query' in message and 'data' in message['callback_query']:
-        # Если в запросе есть callback_query с данными, вызываем by_hourly_event
-        await by_hourly_event(message, db=db)
-
-    return HTTP_200_OK
+        return HTTP_200_OK
+    except Exception as e:
+        print(f"Error handle_bot_events : {e}")
 
 
 def check_secret_key(secret_key: str):
