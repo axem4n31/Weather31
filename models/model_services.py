6b96795dce327e41b7d47d0b0fb0aa9666b5081f
Toporov Denis <toporov.den2@list.ru>
2024-07-20 00:22:35 +0300

    fix create_or_update_user

--- models/model_services.py
+++ models/model_services.py
@@ -2,20 +2,18 @@ from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.model import User
-from models.schemas import CoordinatesSchema, NotificationSchema
+from models.schemas import CoordinatesSchema
 
 
 async def create_or_update_user(
         user_tg_id: int,
         coordinates_data: CoordinatesSchema,
-        notification: NotificationSchema,
         db: AsyncSession
 ) -> bool:
     user = await db.scalar(select(User).where(and_(User.user_tg_id == user_tg_id)))
     if user:
         user.lat = coordinates_data.lat
         user.lon = coordinates_data.lon
-        user.notifications_json = notification.json()
         db.add(user)
         await db.commit()
         return False
@@ -23,7 +21,6 @@ async def create_or_update_user(
         user_tg_id=user_tg_id,
         lat=coordinates_data.lat,
         lon=coordinates_data.lon,
-        notifications_json=notification.json()
     )
     db.add(user)
     await db.commit()
