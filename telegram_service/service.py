a85fadf9975028f4c002fa2192344db1d993138d
Toporov Denis <toporov.den2@list.ru>
2024-07-20 00:24:02 +0300

    fix service func

--- telegram_service/service.py
+++ telegram_service/service.py
@@ -1,25 +1,23 @@
 import json
-
-import httpx
+from typing import Tuple
+from httpx import AsyncClient
 from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession
-
 import settings
 from models.model import User
 
-client = httpx.AsyncClient()
 
-
-async def send_message(chat_id: int, text: str, reply_markup: None | dict):
+async def send_message(chat_id: int, text: str, reply_markup: None | dict) -> None:
     url = settings.TELEGRAM_API_URL + 'sendMessage'
-    reply_markup_json = json.dumps(reply_markup)
+    if reply_markup:
+        reply_markup = json.dumps(reply_markup)
     params = {
         'chat_id': chat_id,
         'text': text,
-        'reply_markup': reply_markup_json
+        'reply_markup': reply_markup
     }
-
-    response = await client.post(url, data=params)
+    async with AsyncClient() as client:
+        await client.post(url, data=params)
 
 
 async def check_user_location(user_tg_id: int, db: AsyncSession) -> bool:
@@ -29,18 +27,18 @@ async def check_user_location(user_tg_id: int, db: AsyncSession) -> bool:
     return False
 
 
-async def get_user_coordinates(user_tg_id: int, db: AsyncSession):
+async def get_user_coordinates(user_tg_id: int, db: AsyncSession) -> Tuple[float, float]:
     user = await db.scalar(select(User).where(and_(User.user_tg_id == user_tg_id)))
     lat = user.lat
     lon = user.lon
     return lat, lon
 
 
-async def delete_message(chat_id: int, message_id: int):
+async def delete_message(chat_id: int, message_id: int) -> None:
     url = settings.TELEGRAM_API_URL + 'deleteMessage'
     params = {
         "chat_id": chat_id,
         "message_id": message_id
     }
-    response = await client.post(url, data=params)
-
+    async with AsyncClient() as client:
+        await client.post(url, data=params)
