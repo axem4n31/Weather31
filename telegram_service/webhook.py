94213d18ae2fedb59f1860b284e01975932c78fb
Toporov Denis <toporov.den2@list.ru>
2024-07-23 16:33:02 +0300

    add ecxept

--- telegram_service/webhook.py
+++ telegram_service/webhook.py
@@ -1,18 +1,25 @@
 import httpx
+from httpx import AsyncClient
 import settings
-
-client = httpx.AsyncClient()
 
 
 async def set_webhook():
-    url = settings.TELEGRAM_API_URL + "setWebhook"
-    url += "?url=" + settings.WEBHOOK_HANDLE_URL
-    url += '&allowed_updates=["message"'
-    url += ', "callback_query"]'
-    response = await client.post(url, timeout=10)
-    print('Webhook set with response:', response.json())
+    try:
+        url = settings.TELEGRAM_API_URL + "setWebhook"
+        url += "?url=" + settings.WEBHOOK_HANDLE_URL
+        url += '&allowed_updates=["message"'
+        url += ', "callback_query"]'
+        async with AsyncClient() as client:
+            response = await client.post(url, timeout=10)
+        print('Webhook set with response:', response.json())
+    except Exception as e:
+        print(f"Error set_webhook : {e}")
 
 
 async def remove_webhook():
-    response = await client.post(settings.TELEGRAM_API_URL + "setWebhook?remove=", timeout=10)
-    print('Webhook deleted with response:', response.json())
+    try:
+        async with AsyncClient() as client:
+            response = await client.post(settings.TELEGRAM_API_URL + "setWebhook?remove=", timeout=10)
+        print('Webhook deleted with response:', response.json())
+    except httpx.ConnectTimeout as e:
+        print(f"Error remove_webhook : {e}")
