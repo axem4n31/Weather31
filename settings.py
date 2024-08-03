8ed7f72820fb05649db43c93e1cf563b9bb0d503
Nikita Profir <neicker44536@gmail.com>
2024-08-03 16:54:01 +0300

    added docker

--- settings.py
+++ settings.py
@@ -6,7 +6,8 @@ load_dotenv()
 
 TOKEN = getenv('TG_TOKEN')
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{TOKEN}/"
-API_URL = getenv('XTUNNEL_URL')
+# API_URL = getenv('XTUNNEL_URL')
+API_URL = getenv('API_URL')
 WEBHOOK_SECRET_KEY = getenv('WEBHOOK_SECRET_KEY')
 WEBHOOK_HANDLE_URL = API_URL + f'/handle_bot_events/{WEBHOOK_SECRET_KEY}/'
 WEATHER_API = 'http://api.weatherapi.com/v1/'
